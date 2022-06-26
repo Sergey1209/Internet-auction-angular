22e354751e295f6e66aca9b82ff8544994040441
Сергей Ивануна <ss1209@yandex.ru>
2022-06-26 07:23:15 +0300

    combobox and authGuard files

---
+++ src/app/components/combo-box/combo-box.component.ts
@@ -0,0 +1,98 @@
+// taken from https://medium.com/@coolsharang/angular-combo-box-840c893a253c
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CategoryLotService } from 'src/app/services/category-lot.service';
+import { CategoryLot } from 'src/app/models/category-lot';
+
+@Component({
+  selector: 'app-combo-box',
+  templateUrl: './combo-box.component.html',
+  styleUrls: ['./combo-box.component.css']
+})
+export class ComboBoxComponent implements OnInit {
+  list: CategoryLot[] = [];
+  filteredList: CategoryLot[] = [];
+  inputItem: string = '';
+  listHidden = true;
+  selectedIndex = -1;
+
+  constructor(private categoryService: CategoryLotService) { }
+
+  ngOnInit() {
+    this.categoryService.getLotCategories().subscribe(listCateg => {
+      this.list = listCateg;
+      this.filteredList = this.list;
+      const index =  this.filteredList.findIndex(x => x.id === this.categoryId);
+      this.selectItem(index);
+    });
+  }
+
+  @Input()
+    categoryId: number = 0;
+
+  @Output()
+    onSelectedCategory = new EventEmitter<number>();
+
+  getFilteredList() {   
+    this.listHidden = false;
+    const filterString = this.inputItem.toLowerCase();
+    if (filterString){
+      if (!this.listHidden && this.inputItem) {
+        this.filteredList = this.list.filter((item) =>  item.name.toLowerCase().includes(filterString));
+      } 
+    }
+    else{
+      this.filteredList = this.list;
+    }
+  }
+  selectItem(ind: number) {
+    if (ind < 0)
+      return;
+
+    this.onSelectedCategory.emit(this.filteredList[ind].id);
+    this.inputItem = this.filteredList[ind].name;
+    this.listHidden = true;
+    this.selectedIndex = ind;
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    if (!this.listHidden) { 
+      if (event.key === 'Escape') {
+        this.selectedIndex = -1;
+        this.toggleListDisplay(0);
+      }else if (event.key === 'Enter') {
+          this.toggleListDisplay(0);
+      }else if (event.key === 'ArrowDown') {
+        this.listHidden = false;
+        this.selectedIndex = (this.selectedIndex + 1) % this.filteredList.length;
+        if (this.filteredList.length > 0 && !this.listHidden) {
+            document.getElementsByTagName('list-item')[this.selectedIndex].scrollIntoView();
+        }
+        
+      } else if (event.key === 'ArrowUp') {
+        this.listHidden = false;
+        if (this.selectedIndex <= 0) {
+            this.selectedIndex = this.filteredList.length;
+        }
+        this.selectedIndex = (this.selectedIndex - 1) % this.filteredList.length;
+        if (this.filteredList.length > 0 && !this.listHidden) {
+          document.getElementsByTagName('list-item')[this.selectedIndex].scrollIntoView();
+        }
+      }
+    }
+  }
+
+  toggleListDisplay(sender: number) {
+    if (sender === 1) {
+      this.listHidden = false;
+      // this.getFilteredList();
+    } else {
+      setTimeout(() => {
+          this.selectItem(this.selectedIndex);
+          this.listHidden = true;
+          this.filteredList = this.list;
+      }, 100);
+    }
+  }
+
+}
